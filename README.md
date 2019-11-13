839f49b3a9fe77935a70b74c7376109350666ce6
Dariusz Janicki <systemy.internetowe@gmail.com>
2019-11-13 11:53:37 +0100

    Start

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # newhttp200pl
 Moja strona firmowa http200.pl
+
+
+F:\PHP7.1\php -S localhost:8100 -t F:\Zlecenia\newhttp200pl\public_html\
---
+++ public_html/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<title>Tworzenie oraz modyfikacja oprogramowania PHP & MySQL. Full Stack Developer. HTTP 200 usługi IT - Dariusz Janicki.</title>
+<style>
+
+.myhero{
+    min-height:550px;
+    background:#004977;
+    background:linear-gradient(#004977 10%, #5D95EC 100%);
+    position:relative;
+}
+
+.myhero .header_bottom{
+    width:100%;
+    height:41px;
+    position:absolute;
+    bottom:0;
+    overflow:hidden
+}
+
+.mybadge{font-size: 95%; padding: .4em .4em;}
+
+</style>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container">	
+		<a class="navbar-brand font-weight-bold" href="/"><span class="badge badge-danger mybadge" >HTTP 200</span> - usługi IT</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Features</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+				<li class="nav-item"><a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<header class="myhero">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">... myhero ....</div>
+		</div>
+	</div>
+	
+	<div class="header_bottom">
+		<!--[if gte IE 9]><!--><svg width="800" height="42" style="width: 100%; height: 42px" viewBox="0 0 800 42" preserveAspectRatio="none"><path d="m0 419.695312c135.135417-26.260416 268.884115-26.260416 401.246094 0 132.361979 26.260417 265.279948 26.260417 398.753906 0v22.199219h-800z" fill="#fff" fill-rule="evenodd" transform="translate(0 -400)"/></svg><!--<![endif]-->
+	</div>
+</header>
+
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<h1>Full Stack Developer</h1>
+		</div>
+	</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
